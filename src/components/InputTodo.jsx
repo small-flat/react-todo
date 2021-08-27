3f38883efabed3effe68ed02422066a1dcd71dc8
small-flat <45090728+small-flat@users.noreply.github.com>
2021-08-27 11:04:03 +0900

    cssファイルからstyleをJSの方へ移動するパターン

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+//cssファイルからstyleをJSの方へ移動するパターン
+//cssファイルをそのまま使う方が良い場合もある。チーム内の選択次第。
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "1rem",
+  margin: "1rem",
+  borderRadius: "0.5rem"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         id="add-text"
         type="text"
